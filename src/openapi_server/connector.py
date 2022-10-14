be65b4aff992837e9827e016506b2d04f3e56016
Maximilinano Osorio <mosorio@isi.edu>
2022-10-14 16:53:08 -0300

    oba integration

---
+++ src/openapi_server/connector.py
@@ -0,0 +1,13 @@
+from obasparql import QueryManager
+from openapi_server.settings import LOGGING_FILE, ENDPOINT, ENDPOINT_GRAPH_BASE, \
+    ENDPOINT_PASSWORD, ENDPOINT_RESOURCE_PREFIX, ENDPOINT_USERNAME, \
+    CONTEXT_DIRECTORY, QUERY_DIRECTORY
+
+query_manager = QueryManager(
+    endpoint=ENDPOINT,
+    endpoint_username=ENDPOINT_USERNAME,
+    endpoint_password=ENDPOINT_PASSWORD,
+    queries_dir=QUERY_DIRECTORY,
+    context_dir=CONTEXT_DIRECTORY,
+    named_graph_base=ENDPOINT_GRAPH_BASE,
+    uri_prefix=ENDPOINT_RESOURCE_PREFIX)
