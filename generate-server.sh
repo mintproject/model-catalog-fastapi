f428b98ff542bc620c5087b47d2da043b8212731
Maximilinano Osorio <mosorio@isi.edu>
2022-10-14 16:28:52 -0300

    add: template

--- generate-server.sh
+++ generate-server.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 set -e
 dir=${PWD}
-docker run -ti --rm -v ${PWD}:/local openapitools/openapi-generator-cli:v6.2.0 \
+docker run \
+    --user "$(id -u):$(id -g)" \
+     -ti --rm -v ${PWD}:/local openapitools/openapi-generator-cli:v6.2.0 \
      generate  \
      -i /local/model-catalog.yaml\
      -g python-fastapi \
      -o /local/ \
+    --template-dir /local/.openapi-generator/template \
      --git-repo-id model-catalog-api \
      --git-user-id mintproject-new 
