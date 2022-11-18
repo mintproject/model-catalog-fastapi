8a6d3c09d7529d5b7b86e098c08bf69f1df2848d
Maximiliano Osorio <mosorio@isi.edu>
2022-11-18 09:31:51 -0300

    fix: primitives handle as union (#6)
    * fix: union type
    
    * fix: typo syntax error
    
    * fix: remove str

---
+++ src/openapi_server/models/union_float_int.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+from __future__ import annotations
+from typing import Union  # noqa: F401
+
+UnionFloatInit = Union[float, int]
---
+++ src/openapi_server/models/union_str_int_none.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+from __future__ import annotations
+from typing import Union
+
+UnionStrIntNone = Union[str, int, None]
